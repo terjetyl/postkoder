d84834204333207e16333135e2de8e6f5f4f7757
terjetyl <terjetyl@gmail.com>
2012-12-30 07:17:14 -0500

    added cors support

--- web.js
+++ web.js
@@ -2,6 +2,13 @@ var express = require('express'),
     zip = require('./routes/zip');
  
 var app = express();
+
+// allow cors requests
+app.all('/*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
  
 app.get('/zip', zip.findAll);
 app.get('/zip/:id', zip.findById);
